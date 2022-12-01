679613c9ce68da994e3417f7fc21369589d07613
Henrik Hellenborg <henrik@hellenborg.com>
2022-12-01 08:35:34 +0100

    Day 1

---
+++ Day1.cs
@@ -0,0 +1,43 @@
+﻿using MoreLinq;
+
+namespace AdventOfCode2022;
+
+public class Day1
+{
+    public object A()
+    {
+        var lines = File.ReadLines("Day1.txt");
+        
+        int maxCals = 0;
+        int cals = 0;
+
+        foreach (var l in lines)
+        {
+            if (string.IsNullOrEmpty(l))
+            {
+                if (cals > maxCals)
+                    maxCals = cals;
+                cals = 0;
+                continue;
+            }
+
+            cals += int.Parse(l);
+        }
+        if (cals > maxCals)
+            maxCals = cals;
+
+        return maxCals;
+    }
+    
+    public object B()
+    {
+        return File
+            .ReadLines("Day1.txt")
+            .Select(l => l == "" ? null : l)
+            .Segment(string.IsNullOrEmpty)
+            .Select(seg => seg.Select(c => int.Parse(c ?? "0")).Sum())
+            .OrderByDescending(o => o)
+            .Take(3)
+            .Sum();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AdventOfCode2022;
+
+DateTime start = DateTime.Now;
+string result = new Day1().B()?.ToString() ?? " ";
+DateTime stop = DateTime.Now;
+
+Console.WriteLine("It took " + (stop - start).TotalSeconds);
+
+WindowsClipboard.SetText(result);
+Console.WriteLine(result);
