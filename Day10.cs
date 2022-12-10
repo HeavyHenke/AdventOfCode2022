3573b2ea36913e1f2e9da23a4fd31889491cc67c
Henrik Hellenborg <henrik@hellenborg.com>
2022-12-10 13:35:55 +0100

    Förbättrat 10:an och förberett för 11:an

---
+++ Day10.cs
@@ -0,0 +1,48 @@
+﻿class Day10
+{
+    public object A()
+    {
+        var xChanges = File.ReadLines("Day10.txt")
+            .SelectMany(l => l.Split(" "))
+            .Select(l => int.TryParse(l, out var parsed) ? parsed : 0);
+
+        int clock = 1;
+        long score = 0;
+        int x = 1;
+        foreach (var cmd in xChanges)
+        {
+            if (clock >= 20 && (clock - 20) % 40 == 0)
+                score += clock * x;
+
+            x += cmd;
+            clock++;
+        }
+
+        return score;
+    }
+
+    public object B()
+    {
+        var xChanges = File.ReadLines("Day10.txt")
+            .SelectMany(l => l.Split(" "))  // addx takes two cycles and becomes two rows
+            .Select(l => int.TryParse(l, out var parsed) ? parsed : 0); // addx, noop => 0
+
+        int clock = 1;
+        int x = 1;
+        foreach (var cmd in xChanges)
+        {
+            var pos = clock % 40;
+            if (pos == 1)
+                Console.WriteLine();
+
+            char chr = pos >= x && pos <= x + 2 ? '#' : '.';
+            Console.Write(chr);
+            
+            x += cmd;
+            clock++;
+        }
+        
+        Console.WriteLine();
+        return "ZKJFBJFZ";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 using AdventOfCode2022;
 
 DateTime start = DateTime.Now;
-string result = new Day10().B()?.ToString() ?? " ";
+string result = new Day11().A()?.ToString() ?? " ";
 DateTime stop = DateTime.Now;
 
 Console.WriteLine("It took " + (stop - start).TotalSeconds);
@@ -12,76 +12,10 @@ Console.WriteLine("It took " + (stop - start).TotalSeconds);
 WindowsClipboard.SetText(result);
 Console.WriteLine(result);
 
-
-class Day10
+class Day11
 {
     public object A()
     {
-        var commands = File.ReadLines("Day10.txt")
-            .Select(l => l.Split(" "))
-            .ToList();
-
-        int cmdIx = 0;
-        int? nextAdd = null;
-        long score = 0;
-        int x = 1;
-        for (int clock = 1; cmdIx < commands.Count; clock++)
-        {
-            if (clock >= 20 && (clock - 20) % 40 == 0)
-                score += clock * x;
-            
-            if (nextAdd.HasValue)
-            {
-                x += nextAdd.Value;
-                nextAdd = null;
-                continue;
-            }
-
-            var cmd = commands[cmdIx++];
-            if (cmd[0] == "addx")
-            {
-                nextAdd = int.Parse(cmd[1]);
-            }
-        }
-        
-        return score;
+        return 0;
     }
-    
-    public object B()
-    {
-        var commands = File.ReadLines("Day10.txt")
-            .Select(l => l.Split(" "))
-            .ToList();
-
-        int cmdIx = 0;
-        int? nextAdd = null;
-        long score = 0;
-        int x = 1;
-        for (int clock = 1; cmdIx < commands.Count; clock++)
-        {
-            var pos = clock % 40;
-            if (pos == 1)
-                Console.WriteLine();
-
-            char chr = pos >= x && pos <= x + 2 ? '#' : '.';
-            Console.Write(chr);
-            
-            if (nextAdd.HasValue)
-            {
-                x += nextAdd.Value;
-                nextAdd = null;
-                continue;
-            }
-
-            var cmd = commands[cmdIx++];
-            if (cmd[0] == "addx")
-            {
-                nextAdd = int.Parse(cmd[1]);
-            }
-        }
-        
-        Console.WriteLine();
-        return "ZKJFBJFZ";
-    }
-
 }
