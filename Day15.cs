e68d6055ccb93d8b7dbabc47a4462ee3594ed69d
Henrik Hellenborg <henrik@hellenborg.com>
2022-12-16 12:51:54 +0100

    Day 16

---
+++ Day15.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+
+class Day15
+{
+    public object A()
+    {
+        var beacons = File.ReadLines("Day15.txt")
+            .Select(r => Regex.Match(r, @"^Sensor at x=(?<sx>-?\d+), y=(?<sy>-?\d+): closest beacon is at x=(?<bx>-?\d+), y=(?<by>-?\d+)$"))
+            .Select(r => (bx: int.Parse( r.Groups["bx"].Value), by: int.Parse(r.Groups["by"].Value), sx: int.Parse(r.Groups["sx"].Value), sy: int.Parse(r.Groups["sy"].Value)))
+            .Select(r => (x: r.sx, y: r.sy, manhattan: Math.Abs(r.bx-r.sx)+Math.Abs(r.by-r.sy), r.bx, r.by))
+            .ToList();
+
+        var xStart = beacons.Min(b => b.x - b.manhattan) - 1;
+        var xStop = beacons.Max(b => b.x + b.manhattan) + 1;
+        int y = 2000000;
+        
+        int numImpossible = 0;
+        for (int x = xStart; x <= xStop; x++)
+        {
+            foreach (var b in beacons)
+            {
+                if (x == b.bx && y == b.by)
+                    break;  // There is a beacon exactly here so not impossible
+                var dist = Math.Abs(x - b.x) + Math.Abs(y - b.y);
+                if (dist <= b.manhattan)
+                {
+                    numImpossible++;
+                    break;
+                }
+            }
+        }
+
+        return numImpossible;
+    }
+    
+    public object B()
+    {
+        var beacons = File.ReadLines("Day15.txt")
+            .Select(r => Regex.Match(r, @"^Sensor at x=(?<sx>-?\d+), y=(?<sy>-?\d+): closest beacon is at x=(?<bx>-?\d+), y=(?<by>-?\d+)$"))
+            .Select(r => (bx: int.Parse( r.Groups["bx"].Value), by: int.Parse(r.Groups["by"].Value), sx: int.Parse(r.Groups["sx"].Value), sy: int.Parse(r.Groups["sy"].Value)))
+            .Select(r => (x: r.sx, y: r.sy, manhattan: Math.Abs(r.bx-r.sx)+Math.Abs(r.by-r.sy), r.bx, r.by))
+            .ToList();
+        
+        int numImpossible = 0;
+        for (int y = 0; y <= 4000000; y++)
+        {
+            // Console.WriteLine("Y: " + y);
+            for (int x = 0; x <= 4000000; x++)
+            {
+                bool possible = true;
+                foreach (var b in beacons)
+                {
+                    if (x == b.bx && y == b.by)
+                    {
+                        possible = false;
+                        break; // There is a beacon exactly here so not impossible
+                    }
+
+                    var dist = Math.Abs(x - b.x) + Math.Abs(y - b.y);
+                    if (dist <= b.manhattan)
+                    {
+                        x = b.x + b.manhattan - Math.Abs(y - b.y);
+                        possible = false;
+                        break;
+                    }
+                }
+
+                if (possible)
+                {
+                    return x * 4000000L + y;
+                }
+            }
+        }
+
+        throw new Exception("not found");
+    }
+
+}
---
+++ Day16.cs
@@ -0,0 +1,154 @@
+﻿using System.Text.RegularExpressions;
+using MoreLinq;
+
+class Day16
+{
+    public object A()
+    {
+        var valves = File.ReadLines("Day16.txt")
+            .Select(l => Regex.Match(l, @"Valve (?<valve>\w+) has flow rate=(?<rate>\d+); tunnels? leads? to valves? (?<next>\w+)(, (?<next>\w+))*"))
+            .Select(m => (valve: m.Groups["valve"].Value, flow: int.Parse(m.Groups["rate"].ValueSpan), next: m.Groups["next"].Captures.Select(c => c.Value).ToArray()))
+            .ToDictionary(key => key.valve);
+
+        var orderedValveFlow = valves.Values.Select(v => (v.valve, v.flow)).OrderByDescending(v => v.flow).ToList();
+
+        var search = new Stack<(HashSet<string> openValves, string pos, int flow, int time, string prevPos)>();
+        search.Push(new(new HashSet<string>(), "AA", 0, 0, ""));
+        int bestSolution = 0;
+        int numVisited = 0;
+
+        while (search.Any())
+        {
+            numVisited++;
+            var node = search.Pop();
+            if (node.flow > bestSolution)
+            {
+                bestSolution = node.flow;
+                Console.WriteLine("Max flow " + bestSolution);
+            }
+            
+            if (node.time == 30)
+                continue;
+
+            var currValvePos = valves[node.pos];
+
+            // Open valve if not open
+            if (node.openValves.Contains(node.pos) == false && currValvePos.flow > 0)
+            {
+                var flowToAdd = currValvePos.flow * (30 - node.time - 1);
+                var openValves = Enumerable.ToHashSet(node.openValves);
+                openValves.Add(node.pos);
+
+                search.Push((openValves, node.pos, node.flow + flowToAdd, node.time + 1, ""));
+            }
+
+            // Is it possible to beat max score from here?
+            var timeLeft = 30 - node.time - 1;
+            var maxNumValvesToOpen = timeLeft / 2;
+            var maxFlowToAdd = orderedValveFlow
+                .Where(v => !node.openValves.Contains(v.valve))
+                .Take(maxNumValvesToOpen)
+                .Index()
+                .Sum(v => v.Value.flow * (30 - node.time - 1 - v.Key * 2));
+            if(node.flow + maxFlowToAdd <= bestSolution)
+                continue;
+
+            // Move
+            foreach (var next in currValvePos.next)
+            {
+                if (next != node.prevPos)
+                    search.Push((node.openValves, next, node.flow, node.time + 1, node.pos));
+            }
+        }
+
+        Console.WriteLine($"Visited {numVisited} nodes");
+
+        return bestSolution;
+    }
+
+    public object B()
+    {
+        var valves = File.ReadLines("Day16.txt")
+            .Select(l => Regex.Match(l, @"Valve (?<valve>\w+) has flow rate=(?<rate>\d+); tunnels? leads? to valves? (?<next>\w+)(, (?<next>\w+))*"))
+            .Select(m => (valve: m.Groups["valve"].Value, flow: int.Parse(m.Groups["rate"].ValueSpan), next: m.Groups["next"].Captures.Select(c => c.Value).ToArray()))
+            .ToDictionary(key => key.valve);
+
+        var orderedValveFlow = valves.Values.Select(v => (v.valve, v.flow)).OrderByDescending(v => v.flow).ToList();
+
+        var search = new Stack<(HashSet<string> openValves, string pos, int flow, int time, string prevPos, string pos2, string prev2)>();
+        search.Push(new(new HashSet<string>(), "AA", 0, 4, "", "AA", ""));
+        int bestSolution = 0;
+        int numVisited = 0;
+
+        while (search.Any())
+        {
+            numVisited++;
+            var node = search.Pop();
+            if (node.flow > bestSolution)
+            {
+                bestSolution = node.flow;
+                Console.WriteLine("Max flow " + bestSolution);
+            }
+            
+            if (node.time == 30)
+                continue;
+
+            // Is it possible to beat max score from here?
+            var timeLeft = 30 - node.time - 2;
+            var maxNumValvesToOpen = timeLeft;
+            var maxFLowThisTurn = (node.openValves.Contains(node.pos) ? 0 : valves[node.pos].flow * (30 - node.time)) + (node.openValves.Contains(node.pos2) ? 0 : valves[node.pos2].flow * (30 - node.time));
+            var maxFlowToAdd = maxFLowThisTurn + orderedValveFlow
+                .Where(v => !node.openValves.Contains(v.valve))
+                .Take(maxNumValvesToOpen)
+                .Index()
+                .Sum(v => v.Value.flow * (30 - node.time - 2 - v.Key * 2));
+            if(node.flow + maxFlowToAdd <= bestSolution)
+                continue;
+
+            var currValvePos = valves[node.pos];
+            var nextMovesForP1 = new List<(HashSet<string> openValves, string pos, int flow, string prevPos)>();
+            
+            // Open valve if not open
+            if (node.openValves.Contains(node.pos) == false && currValvePos.flow > 0)
+            {
+                var flowToAdd = currValvePos.flow * (30 - node.time - 1);
+                var openValves = Enumerable.ToHashSet(node.openValves);
+                openValves.Add(node.pos);
+
+                nextMovesForP1.Add((openValves, node.pos, node.flow + flowToAdd, ""));
+            }
+            
+            // Move
+            foreach (var next in currValvePos.next)
+            {
+                if (next != node.prevPos)
+                    nextMovesForP1.Add((node.openValves, next, node.flow, node.pos));
+            }
+            
+            // Next move for the elephant
+            currValvePos = valves[node.pos2];
+            foreach (var m in nextMovesForP1)
+            {
+                // Open valve if not open
+                if (m.openValves.Contains(node.pos2) == false && currValvePos.flow > 0)
+                {
+                    var flowToAdd = currValvePos.flow * (30 - node.time - 1);
+                    var openValves = Enumerable.ToHashSet(m.openValves);
+                    openValves.Add(node.pos2);
+
+                    search.Push((openValves, m.pos, m.flow + flowToAdd, node.time + 1, m.prevPos, node.pos2, ""));
+                }
+                
+                // Move
+                foreach (var next in currValvePos.next)
+                {
+                    if (next != node.prev2 && !(m.prevPos == node.pos2 && m.prevPos != m.pos && string.Compare(m.pos, next, StringComparison.Ordinal) > 0))
+                        search.Push((m.openValves, m.pos, m.flow, node.time + 1, m.prevPos, next, node.pos2));
+                }
+            }
+        }
+
+        Console.WriteLine($"Visited {numVisited} nodes");
+        return bestSolution;
+    }
+}
