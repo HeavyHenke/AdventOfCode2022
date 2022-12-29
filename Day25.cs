fac6ff2d2807f88e2e583b9d99d38e16f9273ffe
Henrik Hellenborg <henrik@hellenborg.com>
2022-12-29 17:06:14 +0100

    Day 25

---
+++ Day25.cs
@@ -0,0 +1,53 @@
+﻿using MoreLinq.Extensions;
+
+class Day25
+{
+    public object A()
+    {
+        var dict = new Dictionary<char, int>
+        {
+            { '2', 2 },
+            { '1', 1 },
+            { '0', 0 },
+            { '-', -1 },
+            { '=', -2 }
+        };
+        var revDict = dict.ToDictionary(key => key.Value, val => val.Key);
+        var lines = File.ReadAllLines("Day25.txt");
+        var maxLength = lines.Max(l => l.Length);
+
+        var data = lines
+            .Select(l => l.Select(c => dict[c]).PadStart(maxLength))    // Translate to numbers and pad it making all rows equally long
+            .Transpose()
+            .Select(l => l.Sum())   // Sum all columns (since it is transposed now)
+            .Reverse()
+            .ToList();
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            if (data[i] > 0)
+            {
+                var overflow = (data[i] + 2) / 5;
+                data[i] -= overflow * 5;
+                AddValue(data, i + 1, overflow);
+            }
+            else if (data[i] < 0)
+            {
+                var overflow = (data[i] - 2) / 5;
+                data[i] -= overflow * 5;
+                AddValue(data, i + 1, overflow);
+            }
+        }
+
+        var result = new string(data.Select(d => revDict[d]).Reverse().ToArray()).TrimStart('0');
+        return result;
+    }
+
+    private static void AddValue(List<int> base5, int ix, int val)
+    {
+        if (ix > base5.Count - 1)
+            base5.Add(val);
+        else
+            base5[ix] += val;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System;
-using System.Buffers.Text;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Unicode;
-using AdventOfCode2022;
+﻿using AdventOfCode2022;
 
 DateTime start = DateTime.Now;
-string result = new Day24().B()?.ToString() ?? " ";
+string result = new Day25().A()?.ToString() ?? " ";
 DateTime stop = DateTime.Now;
 
 Console.WriteLine("It took " + (stop - start).TotalSeconds);
