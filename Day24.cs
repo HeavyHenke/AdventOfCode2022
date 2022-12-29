cf2c45fd73c797d0e422286968c267bbecefb673
Henrik Hellenborg <henrik@hellenborg.com>
2022-12-29 14:54:27 +0100

    Day 24 part 2

--- Day24.cs
+++ Day24.cs
@@ -10,15 +10,38 @@ class Day24
             .SelectMany(l => l.Value.Index().Select(q => (x: q.Key, y: l.Key, dir: q.Value)))
             .Where(b => b.dir is '<' or '>' or 'v' or '^')
             .ToLookup(key => (key.x, key.y), val => val.dir);
+        
+        var startY = 0;
+        var startX = map[startY].IndexOf('.');
+        var goalY = map.Length - 1;
+        var goalX = map[goalY].IndexOf('.');
 
-        var blizzardMoves = new Dictionary<char, (int dx, int dy, int restartDx, int restartDy)>
-        {
-            { '>', (1, 0, -map[0].Length + 2, 0) },
-            { '<', (-1, 0, map[0].Length - 2, 0) },
-            { '^', (0, -1, 0, map.Length - 2) },
-            { 'v', (0, 1, 0, -map.Length + 2) }
-        };
+        return CalcFastestRoute(startX, startY, goalX, goalY, ref blizzards, map);
+    }
 
+    public object B()
+    {
+        var map = File.ReadAllLines("Day24.txt");
+        var blizzards = map
+            .Index()
+            .SelectMany(l => l.Value.Index().Select(q => (x: q.Key, y: l.Key, dir: q.Value)))
+            .Where(b => b.dir is '<' or '>' or 'v' or '^')
+            .ToLookup(key => (key.x, key.y), val => val.dir);
+        
+        var startY = 0;
+        var startX = map[startY].IndexOf('.');
+        var goalY = map.Length - 1;
+        var goalX = map[goalY].IndexOf('.');
+        
+        return CalcFastestRoute(startX, startY, goalX, goalY, ref blizzards, map) + 1 +
+               CalcFastestRoute(goalX, goalY, startX, startY, ref blizzards, map) + 1 +
+               CalcFastestRoute(startX, startY, goalX, goalY, ref blizzards, map);
+    }
+    
+    
+    private static int CalcFastestRoute(int startX, int startY, int goalX, int goalY, ref ILookup<(int x, int y), char> blizzards, string[] map)
+    {
+        int blizzardsTime = 0;
         var directions = new[]
         {
             (dx: 0, dy: 0),
@@ -27,14 +50,15 @@ class Day24
             (dx: -1, dy: 0),
             (dx: 0, dy: -1)
         };
+        var blizzardMoves = new Dictionary<char, (int dx, int dy, int restartDx, int restartDy)>
+        {
+            { '>', (1, 0, -map[0].Length + 2, 0) },
+            { '<', (-1, 0, map[0].Length - 2, 0) },
+            { '^', (0, -1, 0, map.Length - 2) },
+            { 'v', (0, 1, 0, -map.Length + 2) }
+        };
 
-        var startY = 0;
-        var startX = map[startY].IndexOf('.');
-        var goalY = map.Length - 1;
-        var goalX = map[goalY].IndexOf('.');
-
-        var blizzardsTime = 0;
-
+        
         var visited = new HashSet<(int x, int y, int time)>();
         var searchQueue = new Queue<(int x, int y, int time)>();
         searchQueue.Enqueue((startX, startY, 0));
@@ -43,21 +67,22 @@ class Day24
             var state = searchQueue.Dequeue();
             if (state.x == goalX && state.y == goalY)
                 return state.time;
-            
+
             state.time++;
             if (blizzardsTime < state.time)
             {
-                blizzards = blizzards.SelectMany(b => b.Select(b2 => (pos: b.Key, dir:b2)))
+                blizzards = blizzards.SelectMany(b => b.Select(b2 => (pos: b.Key, dir: b2)))
                     .Select(b => (b.pos, move: blizzardMoves[b.dir], dir: b.dir))
                     .Select(b => (x: b.pos.x + b.move.dx, y: b.pos.y + b.move.dy, b.move, b.dir))
                     .Select(b => map[b.y][b.x] == '#' ? (x: b.x + b.move.restartDx, y: b.y + b.move.restartDy, b.dir) : (b.x, b.y, b.dir))
-                    .ToLookup(key => (key.x , key.y), val => val.dir);
+                    .ToLookup(key => (key.x, key.y), val => val.dir);
 
                 blizzardsTime++;
             }
 
-            foreach (var s in directions.Select(d => (x: state.x + d.dx, y: state.y + d.dy)).Where(pos => pos.y >= 0 && map[pos.y][pos.x] != '#' && blizzards.Contains(pos) == false))
-                if(visited.Add((s.x, s.y, state.time)))
+            var blizzardsNoRef = blizzards;
+            foreach (var s in directions.Select(d => (x: state.x + d.dx, y: state.y + d.dy)).Where(pos => pos.y >= 0 && pos.y < map.Length && map[pos.y][pos.x] != '#' && blizzardsNoRef.Contains(pos) == false))
+                if (visited.Add((s.x, s.y, state.time)))
                     searchQueue.Enqueue((s.x, s.y, state.time));
         }
 
